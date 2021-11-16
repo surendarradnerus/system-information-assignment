e04d894456d0955f6e5ca34180df6e78ed1e1673
surendarradnerus <94035237+surendarradnerus@users.noreply.github.com>
2021-11-17 00:32:42 +0530

    Create sample.js

---
+++ sample.js
@@ -0,0 +1,26 @@
+var input = [{
+    "memorySummary": [{
+        "maximumCapacity": "2048 MBytes",
+        "maximumMemoryModuleSize": "4096 MBytes",
+        "memorySlot": 4,
+        "errorCorrection": "none",
+        "dramFrequency": "199.0 MHz",
+        "memoryTimings": "3-3-3-8(cl-rcd-rp)",
+    }]
+}, {
+    "DeviceLocator":  [{
+         "manufacturer": "hyundai Electronics",
+         "partNumber": "HYMD264 646D8J",
+         "serialNumber": "FFFF7240",
+         "capacity": "512 Mbytes",
+         "memoryType": "DDR(pc3200)",
+         "speed": "200  MHz",
+         "supportedFrequency": "133MHz 166MHz",
+         "memoryTiming": "2-2-2-6 and 133MHz",
+         "memoryTiming": "2-3-3-6 and 166MHz",
+         "dataWidth": "64 Bits",
+         "manufatureDate": "2005 Week39",
+         "eppSpdSupport": "no",
+         "xmpMpdSupport": "no",
+        }]
+    }]
